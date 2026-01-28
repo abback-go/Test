174d04a14467289d3261296c0b6fc227984ec092
abback-go <ababck@mjc.ac.kr>
2026-01-28 14:16:56 +0900

    commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+int a=3;
+int b=5;
+int c= a+b;
+System.Console.WriteLine(c);
